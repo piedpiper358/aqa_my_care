66892f0a405809f79bf620b22190bff71ed665da
Boris <b.sapozhnikov@astondevs.ru>
2024-07-15 14:35:22 +0300

    Добавлены методы refreshPage и setHeadlessDriver в BasePage

--- UI-selenium/src/test/java/core/BasePage.java
+++ UI-selenium/src/test/java/core/BasePage.java
@@ -1,14 +1,27 @@
 package core;
 
+import driver.CreateWebDriver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BasePage {
     protected static WebDriver driver;
+
     public static void setDriver(WebDriver webDriver) {
         driver = webDriver;
     }
-    public static void open(String url) {driver.get(url);}
-    public static String getCurrentUrl() {
-        return driver.getCurrentUrl();
+    public static void setDriver() {
+        //driver = new ChromeDriver();
+        driver = CreateWebDriver.getChrome();
     }
+    public static void setHeadlessDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        driver = new ChromeDriver(options);
+    }
+
+    public void open(String url) {driver.get(url);}
+    public static String getCurrentUrl() { return driver.getCurrentUrl(); }
+    public void refreshPage(){ driver.navigate().refresh();}
 }
